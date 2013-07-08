31e8a23715fe9bde6d820ecfdf4f2ad3ddabdc21
Anand Daniel <anand.daniel@gmail.com>
2013-07-08 17:40:21 +0000

    Updated to use readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
+//var buf = require('buffer');
 var app = express.createServer(express.logger());
+var file = "./index.html";
+
+var data = fs.readFileSync(file,'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
